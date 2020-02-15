ac83d36461b3a081989e020107f98afab74aa8c7
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-02-15 14:33:08 -0500

    Some changes to the reviews

---
+++ GameReviewing/Components/ReviewRenderer.razor.cs
@@ -0,0 +1,17 @@
+﻿using GameReviewing.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameReviewing.Components
+{
+    public partial class ReviewRenderer : ComponentBase
+    {
+        [Parameter]
+        public Review Review { get; set; }
+        [Parameter]
+        public Game Game { get; set; }
+    }
+}
