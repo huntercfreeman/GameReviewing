e93b222328ea82722547c680531f992926e6fbdf
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-02-15 14:46:28 -0500

    beginnings of the star graph rating thing

---
+++ GameReviewing/Components/ReviewStarGraph.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameReviewing.Components
+{
+    public partial class ReviewStarGraph : ComponentBase
+    {
+        [Parameter]
+        public double Rating { get; set; }
+        [Parameter]
+        public double Max { get; set; }
+    }
+}
